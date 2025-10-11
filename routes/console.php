2e733e732b28771e7d90bd26e0f23b9325c01656
raul <rcubitto@gmail.com>
2025-10-11 15:23:55 -0300

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,30 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use App\Models\Expense;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('init', function () {
+    Expense::truncate();
+
+    $path = storage_path('app/private/data.csv');
+
+    $file = fopen($path, 'r');
+
+    if (!$file) {
+        die('Could not open the CSV file.');
+    }
+
+    $csvData = [];
+    while (($row = fgetcsv($file)) !== FALSE) {
+        if ($row[0] === 'Timestamp') {
+            $headers = collect($row)->map(fn ($s) => str($s)->snake())->toArray();
+            continue;
+        }
+
+        $csvData[] = array_combine($headers, $row);
+    }
+
+    fclose($file);
+
+    dd($csvData);
+});
