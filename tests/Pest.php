81611c2420938fd0b902de8630d52edd32e6d497
raul <rcubitto@gmail.com>
2025-10-12 17:12:25 -0300

    Add basic tests (wip)

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,8 +12,12 @@
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature', 'Unit');
+// ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+->in('Feature');
+
+pest()->extend(Tests\TestCase::class)
+    ->in('Unit');
+
 
 /*
 |--------------------------------------------------------------------------
