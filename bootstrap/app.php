bed663546d1d52157c03c040f71d3e4b1fec4fcb
raul <rcubitto@gmail.com>
2025-10-12 19:06:21 -0300

    Fix: prevent re-defining the to_cents() function

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-function to_cents($amount): int
-{
-    $parts = str($amount)->remove([',', '$'])->explode('.');
-    return intval($parts[0].str($parts[1] ?? '')->take(2)->padRight(2, '0'));
+if (! function_exists('to_cents')) {
+    function to_cents($amount): int
+    {
+        $parts = str($amount)->remove([',', '$'])->explode('.');
+        return intval($parts[0].str($parts[1] ?? '')->take(2)->padRight(2, '0'));
+    };
 }
 
 return Application::configure(basePath: dirname(__DIR__))
